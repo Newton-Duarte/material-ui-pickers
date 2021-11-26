55b34f93bfe2b9901a98765070c5371d4b6c80d1
Newton Duarte <newton_@hotmail.com>
2021-11-26 16:13:58 -0300

    feat: add material-ui pickers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+  KeyboardTimePicker,
+} from '@material-ui/pickers';
+
 import './App.css';
 
+const IS_SUNDAY = 0;
+const IS_SATURDAY = 6;
+
+function filterWeekends(date) {
+  return date.getDay() === IS_SUNDAY || date.getDay() === IS_SATURDAY;
+}
+
 function App() {
+  const [date, setDate] = React.useState(new Date());
+  const [time, setTime] = React.useState(new Date());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Material UI - Pickers</h1>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          variant='inline'
+          format='MM/dd/yyyy'
+          label='Material Date Picker'
+          value={date}
+          onChange={setDate}
+          shouldDisableDate={filterWeekends}
+        />
+        <KeyboardTimePicker
+          variant='inline'
+          format='HH:mm'
+          label='Material Time Picker'
+          value={time}
+          onChange={setTime}
+          minutesStep={30}
+        />
+      </MuiPickersUtilsProvider>
     </div>
   );
 }
